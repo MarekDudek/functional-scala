ab7c2976d4c3ae03810628e2d5126d885481cfdd
Marek Dudek <marek.dudek@gmail.com>
2016-01-14 19:38:28 +0100

    Exercises from Chapter 2 done

--- src/main/scala/interretis/functionalscala/introduction/PolymorphicFunction.scala
+++ src/main/scala/interretis/functionalscala/introduction/PolymorphicFunction.scala
@@ -21,4 +21,16 @@ object PolymorphicFunction {
       else loop(i + 1)
     loop(0)
   }
+
+  def partial[A, B, C](a: A, f: (A, B) => C): B => C =
+    b => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
