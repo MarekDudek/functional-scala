537c38a0697beaf43e75723a620c9107e906cbca
Marek Dudek <marek.dudek@gmail.com>
2016-01-17 23:37:22 +0100

    got rid of obsolete constants

--- src/test/scala/interretis/functionalscala/introduction/HigherOrderFunctionsSpec.scala
+++ src/test/scala/interretis/functionalscala/introduction/HigherOrderFunctionsSpec.scala
@@ -5,17 +5,15 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class HigherOrderFunctionsSpec extends FlatSpec with Matchers {
 
-  private val l1to5 = FList(1, 2, 3, 4, 5)
-
   "map" should "work" in {
-    map(l1to5)(_.toString) shouldBe FList("1", "2", "3", "4", "5")
+    map(FList(1, 2, 3, 4, 5))(_.toString) shouldBe FList("1", "2", "3", "4", "5")
   }
 
   "filter" should "work" in {
-    filter(l1to5)(_ % 2 == 0) shouldBe FList(2, 4)
+    filter(FList(1, 2, 3, 4, 5))(_ % 2 == 0) shouldBe FList(2, 4)
   }
 
   "filter2" should "work" in {
-    filter2(l1to5)(_ % 2 == 0) shouldBe FList(2, 4)
+    filter2(FList(1, 2, 3, 4, 5))(_ % 2 == 0) shouldBe FList(2, 4)
   }
 }
