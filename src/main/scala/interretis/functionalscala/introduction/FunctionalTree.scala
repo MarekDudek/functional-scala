19c44d5fb4918a1e4b1637fc9db04b5816456271
Marek Dudek <marek.dudek@gmail.com>
2016-01-21 18:08:56 +0100

    fold on trees

--- src/main/scala/interretis/functionalscala/introduction/FunctionalTree.scala
+++ src/main/scala/interretis/functionalscala/introduction/FunctionalTree.scala
@@ -9,27 +9,45 @@ case class Branch[A](left: FTree[A], right: FTree[A]) extends FTree[A]
 
 object FunctionalTree {
 
-  def treeSize[A](tree: FTree[A]): Int =
-    tree match {
+  def treeSize[A](t: FTree[A]): Int =
+    t match {
       case Leaf(_) => 1
       case Branch(l, r) => 1 + treeSize(l) + treeSize(r)
     }
 
-  def maximum(tree: FTree[Int]): Int =
-    tree match {
+  def treeSize2[A](t: FTree[A]): Int =
+    fold(t)(_ => 1)((l, r) => 1 + l + r)
+
+  def maximum(t: FTree[Int]): Int =
+    t match {
       case Leaf(n) => n
       case Branch(l, r) => maximum(l) max maximum(r)
     }
 
-  def depth[A](tree: FTree[A]): Int =
-    tree match {
+  def maximum2(t: FTree[Int]): Int =
+    fold(t)(identity)((l, r) => l max r)
+
+  def depth[A](t: FTree[A]): Int =
+    t match {
       case Leaf(_) => 0
       case Branch(l, r) => 1 + (depth(l) max depth(r))
     }
 
-  def mapTree[A, B](tree: FTree[A], f: A => B): FTree[B] =
-    tree match {
+  def depth2[A](t: FTree[A]): Int =
+    fold(t)(_ => 0)((l, r) => 1 + (l max r))
+
+  def mapTree[A, B](t: FTree[A], f: A => B): FTree[B] =
+    t match {
       case Leaf(n) => Leaf(f(n))
       case Branch(l, r) => Branch(mapTree(l, f), mapTree(r, f))
+    }
+
+  def mapTree2[A, B](t: FTree[A], f: A => B): FTree[B] =
+    fold(t)(a => Leaf(f(a)): FTree[B])((l: FTree[B], r: FTree[B]) => Branch(l, r))
+
+  def fold[A, B](t: FTree[A])(f: A => B)(m: (B, B) => B): B =
+    t match {
+      case Leaf(n) => f(n)
+      case Branch(l, r) => m(fold(l)(f)(m), fold(r)(f)(m))
     }
 }
--- src/test/scala/interretis/functionalscala/introduction/FunctionalTreeSpec.scala
+++ src/test/scala/interretis/functionalscala/introduction/FunctionalTreeSpec.scala
@@ -1,6 +1,6 @@
 package interretis.functionalscala.introduction
 
-import interretis.functionalscala.introduction.FunctionalTree.{depth, mapTree, maximum, treeSize}
+import interretis.functionalscala.introduction.FunctionalTree.{depth, depth2, mapTree, mapTree2, maximum, maximum2, treeSize, treeSize2}
 import org.scalatest.{FlatSpec, Matchers}
 
 class FunctionalTreeSpec extends FlatSpec with Matchers {
@@ -16,12 +16,28 @@ class FunctionalTreeSpec extends FlatSpec with Matchers {
     treeSize(tree) shouldBe 5
   }
 
+  "leaf" should "be of size 1 (2)" in {
+    treeSize2(leaf) shouldBe 1
+  }
+
+  "tree" should "be of size 5 (2)" in {
+    treeSize2(tree) shouldBe 5
+  }
+
   "leaf" should "max at 5" in {
     maximum(leaf) shouldBe 5
   }
 
   "tree" should "max at 7" in {
     maximum(tree) shouldBe 7
+  }
+
+  "leaf" should "max at 5 (2)" in {
+    maximum2(leaf) shouldBe 5
+  }
+
+  "tree" should "max at 7 (2)" in {
+    maximum2(tree) shouldBe 7
   }
 
   "leaf" should "be 0 deep" in {
@@ -32,6 +48,14 @@ class FunctionalTreeSpec extends FlatSpec with Matchers {
     depth(tree) shouldBe 2
   }
 
+  "leaf" should "be 0 deep (2)" in {
+    depth2(leaf) shouldBe 0
+  }
+
+  "tree" should "be 2 deep (2)" in {
+    depth2(tree) shouldBe 2
+  }
+
   private val doubled = (n: Int) => 2 * n
 
   "leaf" should "map properly" in {
@@ -40,5 +64,13 @@ class FunctionalTreeSpec extends FlatSpec with Matchers {
 
   "tree" should "map properly" in {
     mapTree(tree, doubled) shouldBe Branch(Leaf(10), Branch(Leaf(6), Leaf(14)))
+  }
+
+  "leaf" should "map properly (2)" in {
+    mapTree2(leaf, doubled) shouldBe Leaf(10)
+  }
+
+  "tree" should "map properly (2)" in {
+    mapTree2(tree, doubled) shouldBe Branch(Leaf(10), Branch(Leaf(6), Leaf(14)))
   }
 }
